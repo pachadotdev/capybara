32e4e04e65f7593248b7cd471cbf90a467be0d16
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-01-10 16:25:24 -0500

    tidy dev and src

--- src/03_center_variables.hpp
+++
@@ -1,2 +0,0 @@
-doubles_matrix<> centerVariables_(const doubles_matrix<>& V0, const doubles& w0,
-                                  const list& klist, const double tol);
--- src/04_get_alpha.hpp
+++
@@ -1,2 +0,0 @@
-get_alpha_(const doubles& pi0, const list& klist, const double tol);
-get_alpha_field_(const Col<double>& pi, const list& klist, const double tol);
