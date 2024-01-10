f182db168740d1dbf7e5cc4e0902a0cd0451efbb
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-01-10 16:26:08 -0500

    efficient input conversion and consistent notation for matrices

--- src/02_cpp11_to_from_arma.cpp
+++ src/02_cpp11_to_from_arma.cpp
@@ -1,29 +1,33 @@
 #include "00_main.hpp"
 
 Mat<double> doubles_matrix_to_Mat_(const doubles_matrix<>& A) {
-  int nrows = A.nrow();
-  int ncols = A.ncol();
+  int n = A.nrow();
+  int m = A.ncol();
 
-  Mat<double> B(nrows, ncols);
+  // Before ECE244: Expensive copy
+  // Mat<double> B(n, m);
+  // 
+  // for (int i = 0; i < n; ++i) {
+  //   for (int j = 0; j < m; ++j) {
+  //     double a_ij = A(i, j);
+  //     B(i, j) = a_ij;  // Assuming you can access elements of A in this way
+  //   }
+  // }
 
-  for (int i = 0; i < nrows; ++i) {
-    for (int j = 0; j < ncols; ++j) {
-      double a_ij = A(i, j);
-      B(i, j) = a_ij;  // Assuming you can access elements of A in this way
-    }
-  }
+  // After ECE244: Efficient copy
+  Mat<double> B(const_cast<double*>(REAL(A.data())), n, m, false, false);
 
   return B;
 }
 
 doubles_matrix<> Mat_to_doubles_matrix(const Mat<double>& A) {
-  int nrows = A.n_rows;
-  int ncols = A.n_cols;
+  int n = A.n_rows;
+  int m = A.n_cols;
 
-  writable::doubles_matrix<> B(nrows, ncols);
-
-  for (int i = 0; i < nrows; ++i) {
-    for (int j = 0; j < ncols; ++j) {
+  writable::doubles_matrix<> B(n, m);
+  
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
       double a_ij = A(i, j);
       B(i, j) = a_ij;
     }
@@ -34,18 +38,22 @@ doubles_matrix<> Mat_to_doubles_matrix(const Mat<double>& A) {
 
 Col<double> doubles_to_Vec_(const doubles& x) {
   int n = x.size();
-  Col<double> y(n);
+  
+  // Col<double> y(n);
+  // 
+  // for (int i = 0; i < n; ++i) {
+  //   // double x_i = x[i];
+  //   y(i) = x[i];
+  // }
 
-  for (int i = 0; i < n; ++i) {
-    // double x_i = x[i];
-    y(i) = x[i];
-  }
+  Col<double> y(const_cast<double*>(REAL(x.data())), n, false);
 
   return y;
 }
 
 doubles Vec_to_doubles_(const Col<double>& x) {
   int n = x.n_rows;
+
   writable::doubles y(n);
 
   for (int i = 0; i < n; ++i) {
@@ -58,47 +66,16 @@ doubles Vec_to_doubles_(const Col<double>& x) {
 
 doubles_matrix<> Vec_to_doubles_matrix_(const Col<double>& x) {
   int n = x.n_rows;
+  int m = 1;
+
   // confusing: the dimension is counted as (n, 1) instead of (n, 0) for a
   // column vector
-  writable::doubles_matrix<> Y(n, 1);
-
+  writable::doubles_matrix<> Y(n, m);
+  
   for (int i = 0; i < n; ++i) {
     double x_i = x[i];
     Y(i, 0) = x_i;
   }
-
+ 
   return Y;
 }
-
-// SpMat<double> doubles_matrix_to_SpMat_(const doubles_matrix<>& A) {
-//   int nrows = A.nrow();
-//   int ncols = A.ncol();
-
-//   SpMat<double> B(nrows, ncols);
-
-//   for (int i = 0; i < nrows; ++i) {
-//     for (int j = 0; j < ncols; ++j) {
-//       double a_ij = A(i, j);
-//       B(i, j) = a_ij;  // Assuming you can access elements of A in this
-//       way
-//     }
-//   }
-
-//   return B;
-// }
-
-// doubles_matrix<> SpMat_to_doubles_matrix(const SpMat<double>& A) {
-//   int nrows = A.n_rows;
-//   int ncols = A.n_cols;
-
-//   writable::doubles_matrix<> B(nrows, ncols);
-
-//   for (int i = 0; i < nrows; ++i) {
-//     for (int j = 0; j < ncols; ++j) {
-//       double a_ij = A(i, j);
-//       B(i, j) = a_ij;
-//     }
-//   }
-
-//   return B;
-// }
