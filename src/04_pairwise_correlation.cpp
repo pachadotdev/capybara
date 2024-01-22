8d9dda00c2e3a22c105ab40309cb62ca5dc017a3
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-01-22 13:28:12 -0500

    kendall... simplified

--- src/04_pairwise_correlation.cpp
+++ src/04_pairwise_correlation.cpp
@@ -8,57 +8,8 @@
 // agrees: that is, if either both xi > xj and yi > yj holds or both xi < xj
 // and yi < yj
 
-// test:
-// fit <- capybara::fepoisson(mpg ~ wt + disp | am, data = mtcars)
-// x <- mtcars$mpg
-// y <- predict(fit, type = "response")
-// pairwise_cor_base_ = 0.7580645
-// pairwise_cor_ = 0.7741935
-// base R = 0.7642418
-
-// cor = 2/(n(n-1)) * sum_{i<j} sign(y_i - y_j) * sign(yhat_i - yhat_j)
-// time complexity: O(n^2)
-
-// [[cpp11::register]]
-double pairwise_cor_base_(const doubles& y, const doubles& yhat) {
-  // Auxiliary variables
-
-  int n = y.size();
-  int sign_y;
-  int sign_yhat;
-  int sum_signs = 0;
-
-  // Computation
-
-  for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-      if (y[i] > y[j]) {
-        sign_y = 1;
-      } else if (y[i] < y[j]) {
-        sign_y = -1;
-      } else {
-        sign_y = 0;
-      }
-
-      if (yhat[i] > yhat[j]) {
-        sign_yhat = 1;
-      } else if (yhat[i] < yhat[j]) {
-        sign_yhat = -1;
-      } else {
-        sign_yhat = 0;
-      }
-
-      sum_signs += sign_y * sign_yhat;
-    }
-  }
-
-  double cor = (2.0 / (n * (n - 1))) * sum_signs;
-
-  return cor;
-}
-
 // cor = 1 - 2 * "number of non-concordant pairs" / "factorial of n over 2"
-// time complexity: O(n * log(n)) using trees to count inversions
+// time complexity: O(n * log(n))
 
 class fenwick_tree_ {
   vector<int> BIT;
