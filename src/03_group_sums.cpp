da532ccbbdbc0cf0d48f2a8c8e38494472b598e1
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-02-18 18:53:19 -0500

    fix windows error???

--- src/03_group_sums.cpp
+++ src/03_group_sums.cpp
@@ -12,27 +12,26 @@
   const int P = M.n_cols;
 
   // Auxiliary variables (storage)
-  double denom;
-  int i, j, p, I;
   Mat<double> num(P, 1);
 
   // Compute sum of weighted group sums
-  for (j = 0; j < J; j++) {
+  double denom = 0.0;
+
+  for (int j = 0; j < J; j++) {
     // Subset j-th group
     integers indexes = as_cpp<integers>(jlist[j]);
-    I = indexes.size();
+    int I = indexes.size();
 
     // Compute numerator of the weighted group sum
     num.zeros();
-    for (p = 0; p < P; ++p) {
-      for (i = 0; i < I; i++) {
+    for (int p = 0; p < P; ++p) {
+      for (int i = 0; i < I; i++) {
         num(p, 0) += M(indexes[i], p);
       }
     }
 
     // Compute denominator of the weighted group sum
-    denom = 0.0;
-    for (i = 0; i < I; i++) {
+    for (int i = 0; i < I; i++) {
       denom += w(indexes[i]);
     }
   }
@@ -42,10 +41,9 @@
   return as_doubles_matrix(num);
 }
 
-[[cpp11::register]] doubles_matrix<>
-group_sums_spectral_(const doubles_matrix<> &M_r, const doubles_matrix<> &v_r,
-                     const doubles_matrix<> &w_r, const int K,
-                     const list &jlist) {
+[[cpp11::register]] doubles_matrix<> group_sums_spectral_(
+    const doubles_matrix<> &M_r, const doubles_matrix<> &v_r,
+    const doubles_matrix<> &w_r, const int K, const list &jlist) {
   // Types conversion
   Mat<double> M = as_Mat(M_r);
   Mat<double> v = as_Mat(v_r);
@@ -56,29 +54,28 @@ group_sums_spectral_(const doubles_matrix<> &M_r, const doubles_matrix<> &v_r,
   const int P = M.n_cols;
 
   // Auxiliary variables (storage)
-  double denom;
-  int i, j, k, p, I;
   Mat<double> num(P, 1);
 
   // Compute sum of weighted group sums
-  for (j = 0; j < J; j++) {
+  double denom = 0.0;
+
+  for (int j = 0; j < J; j++) {
     // Subset j-th group
     integers indexes = as_cpp<integers>(jlist[j]);
-    I = indexes.size();
+    int I = indexes.size();
 
     // Compute numerator of the weighted group sum given bandwidth 'L'
     num.zeros();
-    for (p = 0; p < P; p++) {
-      for (k = 1; k <= K; k++) {
-        for (i = k; i < I; i++) {
+    for (int p = 0; p < P; p++) {
+      for (int k = 1; k <= K; k++) {
+        for (int i = k; i < I; i++) {
           num(p, 0) += M(indexes[i], p) * v(indexes[i - k]) * I / (I - k);
         }
       }
     }
 
     // Compute denominator of the weighted group sum
-    denom = 0.0;
-    for (i = 0; i < I; i++) {
+    for (int i = 0; i < I; i++) {
       denom += w(indexes[i]);
     }
   }
@@ -88,8 +85,8 @@ group_sums_spectral_(const doubles_matrix<> &M_r, const doubles_matrix<> &v_r,
   return as_doubles_matrix(num);
 }
 
-[[cpp11::register]] doubles_matrix<>
-group_sums_var_(const doubles_matrix<> &M_r, const list &jlist) {
+[[cpp11::register]] doubles_matrix<> group_sums_var_(
+    const doubles_matrix<> &M_r, const list &jlist) {
   // Types conversion
   Mat<double> M = as_Mat(M_r);
 
@@ -98,21 +95,20 @@ group_sums_var_(const doubles_matrix<> &M_r, const list &jlist) {
   const int P = M.n_cols;
 
   // Auxiliary variables (storage)
-  int i, j, p, I;
   Mat<double> v(P, 1);
   Mat<double> V(P, P);
 
   // Compute covariance matrix
   V.zeros();
-  for (j = 0; j < J; ++j) {
+  for (int j = 0; j < J; ++j) {
     // Subset j-th group
     integers indexes = as_cpp<integers>(jlist[j]);
-    I = indexes.size();
+    int I = indexes.size();
 
     // Compute group sum
     v.zeros();
-    for (p = 0; p < P; p++) {
-      for (i = 0; i < I; ++i) {
+    for (int p = 0; p < P; p++) {
+      for (int i = 0; i < I; ++i) {
         v(p) += M(indexes[i], p);
       }
     }
@@ -124,9 +120,9 @@ group_sums_var_(const doubles_matrix<> &M_r, const list &jlist) {
   return as_doubles_matrix(V);
 }
 
-[[cpp11::register]] doubles_matrix<>
-group_sums_cov_(const doubles_matrix<> &M_r, const doubles_matrix<> &N_r,
-                const list &jlist) {
+[[cpp11::register]] doubles_matrix<> group_sums_cov_(
+    const doubles_matrix<> &M_r, const doubles_matrix<> &N_r,
+    const list &jlist) {
   // Types conversion
   Mat<double> M = as_Mat(M_r);
   Mat<double> N = as_Mat(N_r);
@@ -136,21 +132,20 @@ group_sums_cov_(const doubles_matrix<> &M_r, const doubles_matrix<> &N_r,
   const int P = M.n_cols;
 
   // Auxiliary variables (storage)
-  int i, j, p, q, s, I;
   Mat<double> V(P, P);
 
   // Compute covariance matrix
   V.zeros();
-  for (j = 0; j < J; ++j) {
+  for (int j = 0; j < J; ++j) {
     // Subset j-th group
     integers indexes = as_cpp<integers>(jlist[j]);
-    I = indexes.size();
+    int I = indexes.size();
 
     // Add to covariance matrix
-    for (p = 0; p < P; p++) {
-      for (q = 0; q < P; q++) {
-        for (i = 0; i < I; i++) {
-          for (s = i + 1; s < I; s++) {
+    for (int p = 0; p < P; p++) {
+      for (int q = 0; q < P; q++) {
+        for (int i = 0; i < I; i++) {
+          for (int s = i + 1; s < I; s++) {
             V(q, p) += M(indexes[i], q) * N(indexes[s], p);
           }
         }
