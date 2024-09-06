b66ee27ae90c5d230e9a3975951e3cc23f1dc8ba
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-09-05 20:00:45 -0400

    add confint method

---
+++ R/generics_confint.R
@@ -0,0 +1,23 @@
+#' @export
+#' @noRd
+confint.feglm <- function(object, parm, level = 0.95, ...) {
+  # Extract the summary of the feglm object
+  res <- summary(object)$cm
+  colnames(res) <- c("estimate", "std.error", "statistic", "p.value")
+
+  # Calculate the critical value for the specified confidence level
+  alpha <- 1 - level
+  z <- qnorm(1 - alpha / 2)
+
+  # Compute the confidence intervals
+  conf.int <- data.frame(
+    conf.low = res[, "estimate"] - z * res[, "std.error"],
+    conf.high = res[, "estimate"] + z * res[, "std.error"]
+  )
+
+  colnames(conf.int) <- paste(100 * (c(0, 1) + c(1, -1) * (1 - conf.level) / 2),
+    "%")
+
+  # Return the confidence intervals
+  conf.int
+}
