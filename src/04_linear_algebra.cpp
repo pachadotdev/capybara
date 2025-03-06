c7a09a1f2732d83c462f65661bcc4eda38d2607a
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2025-03-05 20:11:05 -0500

    refactor algebra

--- src/04_linear_algebra.cpp
+++ src/04_linear_algebra.cpp
@@ -1,36 +1,19 @@
 #include "00_main.h"
 
-Mat<double> crossprod_(const Mat<double> &X, const Col<double> &w, const int &n,
-                       const int &p, const bool &weighted,
-                       const bool &root_weights) {
-  Mat<double> res(p, p);
-
-  if (weighted == false) {
-    res = X.t() * X;
-  } else {
-    Mat<double> Y(n, p);
-    if (root_weights == false) {
-      Y = X.each_col() % w;
-    } else {
-      Y = X.each_col() % sqrt(w);
-    }
-    res = Y.t() * Y;
-  }
-
-  return res;
+Mat<double> crossprod_(const Mat<double> &X, const Col<double> &w) {
+  Mat<double> Y = X;
+  Y.each_col() %= sqrt(w);
+  return Y.t() * Y;
 }
 
-Col<double> solve_beta_(const Mat<double> &MX, const Mat<double> &MNU,
+Col<double> solve_beta_(Mat<double> MX, const Mat<double> &MNU,
                         const Col<double> &w) {
-  Col<double> wtilde = sqrt(w);
+  MX.each_col() %= sqrt(w);
 
   Mat<double> Q, R;
-
-  bool computable = qr_econ(Q, R, MX.each_col() % wtilde);
-
-  if (!computable) {
+  if (!qr_econ(Q, R, MX)) {
     stop("QR decomposition failed");
   }
 
-  return solve(R, Q.t() * (MNU.each_col() % wtilde));
+  return solve(R, Q.t() * (MNU.each_col() % sqrt(w)));
 }
