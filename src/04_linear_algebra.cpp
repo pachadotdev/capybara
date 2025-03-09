91b5fadbdac880b9d5d75d25f8f0e4b9ba2519d6
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2025-03-08 23:22:42 -0500

    indicate that R is triangular in solve beta

--- src/04_linear_algebra.cpp
+++ src/04_linear_algebra.cpp
@@ -16,5 +16,25 @@ Col<double> solve_beta_(Mat<double> MX, const Mat<double> &MNU,
     stop("QR decomposition failed");
   }
 
-  return solve(R, Q.t() * (MNU.each_col() % sqrt_w));
+  return solve(trimatu(R), Q.t() * (MNU.each_col() % sqrt_w));
 }
+
+// Col<double> solve_beta_(Mat<double> MX, const Mat<double> &MNU,
+//                         const Col<double> &w) {
+//   const Col<double> sqrt_w = sqrt(w);
+//   MX.each_col() %= sqrt_w;
+
+//   // Compute X'X (Gram matrix) and X'Y
+//   Mat<double> XtX = MX.t() * MX;
+//   Col<double> XtY = MX.t() * (MNU.each_col() % sqrt_w);
+
+//   // LDLT decomposition (faster than QR)
+//   Col<double> beta;
+//   bool success = solve(beta, XtX, XtY, solve_opts::fast);
+
+//   if (!success) {
+//     stop("LDLT decomposition failed");
+//   }
+
+//   return beta;
+// }
